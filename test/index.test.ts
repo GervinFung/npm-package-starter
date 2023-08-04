024791db733e66c8deb95cb206a17877d0bb8a85
PoolOfDeath20 <53143214+GervinFung@users.noreply.github.com>
2023-08-04 17:22:36 +0800

    Update package GitHub actions (#2)
    * refactor(*): update packages & github actions
    
    * refactor(*): formatted all files with new prettier
    
    * lint(eslint): put `eslint` in script

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, it } from 'vitest';
 import main from '../src';
 
 describe('should test', () => {
-    it('should parse, ignore comments, and generate type definitions', () => {
-        expect(main()).toBe(1);
-    });
+	it('should parse, ignore comments, and generate type definitions', () => {
+		expect(main()).toBe(1);
+	});
 });
